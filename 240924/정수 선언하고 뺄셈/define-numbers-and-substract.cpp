7daaf507e884be6057521ed037d77c83d6ba42e8
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 13:54:11 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num1 = 97;
+    int num2 = 13;
+    cout << num1 << " - " << num2 << " = " << num1 - num2;
+    return 0;
+}
