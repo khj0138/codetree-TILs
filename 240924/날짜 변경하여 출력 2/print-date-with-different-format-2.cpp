60d81c0bfac6cc540793344a5f5b81c1a6584672
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 14:47:17 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int mm, dd, yyyy;
+    char c1, c2;
+    cin >> mm >> c1 >> dd >> c2 >> yyyy;
+    cout << yyyy << '.' << mm << '.' << dd;
+    return 0;
+}
