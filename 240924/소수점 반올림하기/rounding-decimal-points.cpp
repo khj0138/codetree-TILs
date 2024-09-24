d98b1e7b04a7378a1ef903e78d482dcebe6d8d9a
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 14:07:47 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float a = 25.352f;
+    cout << fixed;
+    cout.precision(1);
+    cout << a;
+    return 0;
+}
