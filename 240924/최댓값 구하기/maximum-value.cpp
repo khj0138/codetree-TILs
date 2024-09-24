2fc31a60d7ad0272527bc703f33d866ea715e1fc
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 17:13:24 +0900

    Title: 최댓값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    int max = a;
+    if (max < b)
+    {
+        max = b;
+    }
+    if(max < c)
+    {
+        max = c;
+    }
+    cout << max;
+    return 0;
+}
