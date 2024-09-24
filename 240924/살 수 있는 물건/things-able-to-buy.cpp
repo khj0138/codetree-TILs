151bbb90e1b725ae9eb498023ca2575d2d9bd5cf
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 16:09:11 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/살 수 있는 물건/things-able-to-buy.cpp
+++ 240924/살 수 있는 물건/things-able-to-buy.cpp
@@ -5,6 +5,6 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    cout << (n >= 3000 ? "book" : (n >= 1000 ? "mask" : ""));
+    cout << (n >= 3000 ? "book" : (n >= 1000 ? "mask" : "no"));
     return 0;
 }
