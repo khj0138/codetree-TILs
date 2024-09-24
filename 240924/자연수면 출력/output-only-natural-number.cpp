cece63b1f9709743698190bf0829d12a384dde99
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 18:36:07 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/자연수면 출력/output-only-natural-number.cpp
+++ 240924/자연수면 출력/output-only-natural-number.cpp
@@ -5,13 +5,16 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     cin >> a >> b;
-    if (a <= 0)
+    if (a > 0)
     {
-        a = 0;
+        for (int i = 0; i < b; i++)
+        {
+            cout << a;
+        }
     }
-    for (int i = 0; i < b; i++)
+    else
     {
-        cout << a;
+        cout << 0;
     }
     return 0;
 }
