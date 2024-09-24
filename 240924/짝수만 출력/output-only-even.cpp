86ea00c0545742d46efd922cbb5edfc4649d396b
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 17:43:37 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    if (a % 2 != 0)
+    {
+        a++;
+    }
+    int i = a;
+    while(i <= b)
+    {
+        cout << i << ' ';
+        i += 2;
+    }
+    return 0;
+}
