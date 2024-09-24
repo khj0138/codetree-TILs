9ac90d7af3f44eea6cdf87c005b1bcd870b37c4c
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 16:56:38 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240924/장학금/scholarship.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    int price = 0;
+    if(a >= 90)
+    {
+        if (b >= 95)
+        {
+            price = 10;
+        }
+        else if (b >= 90)
+        {
+            price = 5;
+        }
+    }
+    cout << price;
+    return 0;
+}
