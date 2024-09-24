9e6886eba55f9e0fa6303442b56ce3f907f3bd43
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 14:33:58 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a, b;
+    cin >> a >> b;
+    double sum = a + b;
+    cout << fixed;
+    cout.precision(2);
+    cout << sum;
+    return 0;
+}
