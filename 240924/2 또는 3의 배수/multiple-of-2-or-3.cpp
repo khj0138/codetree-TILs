ff1e4f521974b9e65c7370a9772d8fb543359a07
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-24 18:38:17 +0900

    Title: 2 또는 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/2 또는 3의 배수/multiple-of-2-or-3.cpp
+++ 240924/2 또는 3의 배수/multiple-of-2-or-3.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         if(i % 2 == 0 || i % 3 == 0)
         {
