f686d7122fd20f8e879950f2f930954b104c1570
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-25 20:46:04 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240925/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum = 0;
+    int a, b;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++)
+    {
+        sum += i;
+    }
+    cout << sum;
+    return 0;
+}
