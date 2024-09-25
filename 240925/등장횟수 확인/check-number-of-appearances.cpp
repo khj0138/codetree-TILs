c30deda8e7fd6cb375e0155d77b16e778b75b997
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-25 20:40:42 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240925/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt = 0;
+    int n = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> n;
+        if(n % 2 == 0)
+        {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
