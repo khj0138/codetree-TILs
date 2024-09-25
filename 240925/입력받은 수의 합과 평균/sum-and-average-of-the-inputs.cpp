fb4301bdb13cfacb69288b4e4a78ef4efc3b12f6
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-09-25 21:09:02 +0900

    Title: 입력받은 수의 합과 평균, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240925/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int num;
+    int cnt = 0;
+    int sum = 0;
+    double avr = 0;
+    for (int i = 0 ; i < n; i++)
+    {
+        cin >> num;
+        sum += num;
+        cnt++;
+    }
+    if(cnt > 0)
+    {
+        avr = (double)sum / (double)cnt;
+    }
+    cout << sum << ' ' << avr;
+    return 0;
+}
