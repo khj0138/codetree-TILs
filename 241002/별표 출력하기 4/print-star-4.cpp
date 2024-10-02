161691b1323ebc57d7d6279132a8fe9955d84e0e
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-02 21:40:52 +0900

    Title: 별표 출력하기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241002/별표 출력하기 4/print-star-4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int check = 1;
+    for (int i = 1; i >= 1; i = i + check)
+    {
+        if(i == n)
+        {
+            check = -1;
+        }
+        for(int j = 0; j <= n - i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
