545286b486f3d862ccdc853d3db54664813066ef
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-02 21:13:15 +0900

    Title: ab 사이에 있는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241002/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    bool check = false;
+
+    for (int i = a; i <= b; i++)
+    {
+        if(i % c == 0)
+        {
+            check = true;
+            break;
+        }
+    }
+    if(check)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
