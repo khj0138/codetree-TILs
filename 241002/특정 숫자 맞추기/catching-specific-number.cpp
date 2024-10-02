88946e2c2981325b66e945cb5f18c93b9f2651f6
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-02 20:17:35 +0900

    Title: 특정 숫자 맞추기, Time: 1046ms, Memory: 65MB, Status: Time Limit Exceed - CodeTree

---
+++ 241002/특정 숫자 맞추기/catching-specific-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = -1;
+    bool check = false;
+    while(!check)
+    {
+        cin >> n;
+        if(n < 25)
+        {
+            cout << "Higher" << endl;
+        }
+        else if (n > 25)
+        {
+            cout << "Lower" << endl;
+        }
+        else
+        {
+            cout << "Good";
+        }
+    }
+    return 0;
+}
