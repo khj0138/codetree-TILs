7125f538a08280edd0c9bdecf68d0e4e1bf86bec
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-02 21:34:31 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241002/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 241002/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= (2 * i + 1); j++)
+        for (int j = 0; j < (2 * i + 1); j++)
         {
             cout << '*';
         }
