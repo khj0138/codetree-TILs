3f3edc6097e7bfc50cf26b1e7c91b9a022abf5e7
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-02 21:43:46 +0900

    Title: 건너뛰고 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241002/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int check = 1;
+    for (int i = 1; i >= 1; i = i + check)
+    {
+        if(i == n)
+        {
+            check = -1;
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
