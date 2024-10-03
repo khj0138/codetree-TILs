a2f1112ec952aecd07a3aeb0d699af3a1903d5ae
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-03 21:32:03 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/별표 출력하기 12/print-star-12.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(i == 0)
+            {
+                cout << "* ";
+            }
+            else if(j % 2 != 0 && j >= i)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
