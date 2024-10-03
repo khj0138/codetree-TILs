37c4836d667ece32bc0f0ff246932121361f6115
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-03 21:22:01 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int iCheck = 1;
+    int jCheck = 0;
+    for(int i = 0; i >= 0; i += iCheck)
+    {
+        if(i >= (n-1))
+        {
+            if(iCheck == 0)
+            {
+                iCheck = -1;
+            }
+            else
+            {
+                iCheck = 0;
+            }
+        }
+
+        if(i % 2 == 0)
+        {
+            jCheck = n - (i / 2) - 1;
+        }
+        else
+        {
+            jCheck = (i / 2);
+        }
+        for (int j = 0; j <= jCheck; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
