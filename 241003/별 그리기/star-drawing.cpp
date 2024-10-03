77eb1145e64cb6b6fbddac27b92db2ecf44bc458
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-03 19:53:25 +0900

    Title: 별 그리기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/별 그리기/star-drawing.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int iCheck = 1;
+    int jCheck = 1;
+    for (int i = 0; i >= 0; i += iCheck)
+    {
+        if(i >= (n-1))
+        {
+            iCheck = -1;
+        }
+        jCheck = 1;
+        for (int j = 0; j >= 0 ; j += jCheck)
+        {
+            if(j >= (n-1))
+            {
+                jCheck = -1;
+            }
+            if (j >= (n - 1 - i))
+            {
+                cout << '*';
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
