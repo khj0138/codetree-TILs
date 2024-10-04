fdc7d75957315af4190c1936dee8b0db2319194a
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-04 13:46:55 +0900

    Title: 좌우 지그재그 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241004/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int outNum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                outNum = (i - 1) * n + j 
+                cout << outNum << endl;
+            }
+        }
+        else
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                outNum = (i - 1) * n + (n + 1 - j)
+                cout << outNum << endl;
+            }
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
