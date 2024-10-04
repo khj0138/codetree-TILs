96519823b7899660d2cb12e45e3668945d0ec750
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-04 14:09:02 +0900

    Title: 숫자 피라미드 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241004/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int sum = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << sum++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
