f10ce84ab1b04b326fdefda01e38b3fb4b8c67fd
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-04 14:37:53 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241004/3n + 1 수열 m번/3n-plus-1-sequence-m-times.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m;
+    cin >> m;
+    int n;
+    int cnt;
+    for (int i = 0; i <= m; i++)
+    {
+        cin >> n;
+        while (n != 1)
+        {
+            cnt++;
+            if(n % 2 == 0)
+            {
+                n /= 2;
+            }
+            else
+            {
+                n = n * 3 + 1;
+            }
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
