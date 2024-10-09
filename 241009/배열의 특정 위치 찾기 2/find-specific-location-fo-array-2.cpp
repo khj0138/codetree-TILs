df8761d0a87bab636d9d2fd192a1af2ca99833c0
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-09 19:35:23 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241009/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int large = 0, small = 0;
+    int n;
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> n;
+        if(i % 2 != 0)
+        {
+            large += n;
+        }
+        else
+        {
+            small += n;
+        }
+    }
+    if(large < small)
+    {
+        int temp = large;
+        large = small;
+        small = large;
+    }
+    cout << large - small;
+    return 0;
+}
