aa485a814795a7ccda747acd4acdf5d806e2f2a0
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-09 20:14:47 +0900

    Title: 전항의 두 배, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241009/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int* arr = new int[10];
+    cin >> arr[0] >> arr[1];
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i + 2] = arr[i] * 2 + arr[i + 1];
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    delete arr;
+    return 0;
+}
