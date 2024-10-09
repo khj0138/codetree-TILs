706960937b03d18e81577c75aa57bfaaab1d818d
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-09 19:33:25 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241009/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char* c = new char[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> c[i];
+    }
+    for (int i = 1; i <= 7; i += 3)
+    {
+        cout << c[i] << ' ';
+    }
+    
+    return 0;
+}
