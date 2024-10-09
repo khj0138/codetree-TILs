4ca5333bde3f049c66d4cc7a750b5a71e1062c8a
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-09 21:41:14 +0900

    Title: 가장 큰 수 고르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241009/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int* arr = new int[10];
+
+    int max = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+        if(max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    cout << max;
+    delete arr;
+    return 0;
+}
