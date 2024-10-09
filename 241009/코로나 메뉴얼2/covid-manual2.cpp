2429e080c0a771a723db9c7b939255a69aebf34c
Hyunjun Kang <86392148+khj0138@users.noreply.github.com>
2024-10-09 20:31:42 +0900

    Title: 코로나 메뉴얼2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241009/코로나 메뉴얼2/covid-manual2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int* arr = new int[4];
+    char c;
+    double n;
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> c >> n;
+        if(c == 'Y')
+        {
+            if(n >= 37.f)
+            {
+                arr[0]++;
+            }
+            else
+            {
+                arr[2]++;
+            }
+        }
+        else
+        {
+            if(n >= 37.f)
+            {
+                arr[1]++;
+            }
+            else
+            {
+                arr[3]++;
+            }
+        }
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    if(arr[0] >= 2)
+    {
+        cout << 'E';
+    }
+    delete arr;
+    return 0;
+}
